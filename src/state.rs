86ce2dd81fac18a8db5a95c5f69da72ee6605e8e
StoyK <samokukata@gmail.com>
2022-07-15 00:21:23 +0300

    300 lines of code for an orange triangle

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,11 @@
 use wgpu::{
-    Backends, Color, CommandEncoderDescriptor, Device, DeviceDescriptor, Features, Instance,
-    Limits, LoadOp, Operations, PowerPreference, PresentMode, Queue, RenderPassColorAttachment,
-    RenderPassDescriptor, RequestAdapterOptions, Surface, SurfaceConfiguration, SurfaceError,
-    TextureUsages, TextureViewDescriptor,
+    Backends, BlendState, Color, ColorTargetState, ColorWrites, CommandEncoderDescriptor, Device,
+    DeviceDescriptor, Face, Features, FragmentState, FrontFace, Instance, Limits, LoadOp,
+    MultisampleState, Operations, PipelineLayoutDescriptor, PolygonMode, PowerPreference,
+    PresentMode, PrimitiveState, PrimitiveTopology, Queue, RenderPassColorAttachment,
+    RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor, RequestAdapterOptions,
+    ShaderModuleDescriptor, ShaderSource, Surface, SurfaceConfiguration, SurfaceError,
+    TextureUsages, TextureViewDescriptor, VertexState,
 };
 use winit::{dpi::PhysicalSize, event::WindowEvent, window::Window};
 
@@ -12,6 +15,7 @@ pub struct State {
     queue: Queue,
     config: SurfaceConfiguration,
     pub size: PhysicalSize<u32>,
+    render_pipeline: RenderPipeline,
 }
 
 impl State {
@@ -78,12 +82,121 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        // A [ShaderModule] represents a compiled shader program
+        // that we include through the file "shader.wgsl"
+        // NOTE: All of this can be replaced with a macro like so:
+        // ```
+        // let shader = device.create_shader_module(include_wgsl!("shader.wgsl"))
+        // ```
+        let shader = device.create_shader_module(ShaderModuleDescriptor {
+            // Label for debugging purposes
+            label: Some("Shader"),
+            // The source of the file a.k.a. where it's located related to "state.rs"
+            source: ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        // A [PipelineLayout] represents the mapping and placing in memory between all [BindGroup]s
+        // [BindGroup]s are groups of resources bound together in a group (unexpected, right?)
+        // these resources can be textures, samplers(encoders for transformations) and bindings
+        // to buffers
+        let render_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            // Label for debugging purposes
+            label: Some("Render Pipeline Layout"),
+            // [BindGroup]s that the pipeline uses
+            bind_group_layouts: &[],
+            // [PushConstantRange] is a set of push constants, which are small amounts of data
+            // we can send directly to a [ShaderModule] and any of its stages(Vertex, Fragment...)
+            push_constant_ranges: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+            // Label for debugging purposes
+            label: Some("Render Pipeline"),
+            // Give the render pipeline a [PipelineLayout]
+            layout: Some(&render_pipeline_layout),
+            // Define a [VertexState] or a description of our vertex shader
+            vertex: VertexState {
+                // Give the actual variable holding the [ShaderModule]
+                module: &shader,
+                // Define the name(entry_point) of the function for the vertex shader
+                // in the file
+                entry_point: "vs_main",
+                // Give the [VertexBufferLayout](buffers of vertices) we use if there are any
+                // In our case we have defined them in "shader.wgsl" so there is no reason to
+                // add any here
+                buffers: &[],
+            },
+            // Define a [FragmentState] or a description of our fragment shader
+            // It's technically optional, so we wrap it in Some()
+            fragment: Some(FragmentState {
+                // Give the actual variable holding the [ShaderModule]
+                module: &shader,
+                // Define the name(entry_point) of the function for the fragment
+                // shader in the file
+                entry_point: "fs_main",
+                // A [ColorTargetState] defines how the GPU should render colors
+                targets: &[Some(ColorTargetState {
+                    // The [TextureFormat] for the [ColorTargetState] must be the same
+                    // as the [TextureFormat] used in the [CommandEncoder]'s begin_render_pass function
+                    // That's why we are using the [TextureFormat] from the config we previously used
+                    format: config.format,
+                    // The [BlendState] defines how colors from previous renders should blend, in our
+                    // case we don't want them to blend so we tell the GPU to just replace the old colors
+                    blend: Some(BlendState::REPLACE),
+                    // The [ColorWrites] or write mask tells the GPU if it should ignore any parts of the
+                    // RGBA colors, in our case we want to use all colors possible so we don't use a color mask
+                    write_mask: ColorWrites::ALL,
+                })],
+            }),
+            // The [PrimitiveState] describes how the GPU should handle converting
+            // our vertices to triangles
+            primitive: PrimitiveState {
+                // The [PrimitiveTopology] describes how to group different vertices
+                // For example:
+                // PrimitiveTopology::PointList tells the GPU to treat every verticie as a new point
+                // PrimitiveTopology::TriangleList tells the GPU to treat vertices as a group of 3
+                // a.k.a. triangles
+                topology: PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                // [FrontFace] tells the GPU how to determine if a triangle is front facing or not
+                // FrontFace::Ccw means that a triangle is front facing if the vertices of said
+                // triangle are aranged in a counter-clockwise direction
+                front_face: FrontFace::Ccw,
+                // [Face] cull_mode tells the GPU that triangles that are not front facing are
+                // culled or said in simpler terms - not included in the rendering
+                cull_mode: Some(Face::Back),
+                // [PolygonMode] tells the GPU how to render polygons either Fill, Line or Point
+                // Fill renders the vertices, the lines between the vertices and what is inside the actual polygon
+                // Line renders the vertices and the lines between the vertices
+                // Point only renders the vertices
+                polygon_mode: PolygonMode::Fill,
+                // When rasterization occurs, the GPU has to decide what shapes are above others in a depth scale
+                // unclipped_depth unclamps the depth for polygons from only 0-1 to any numbers
+                // Enabling this requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // When rasterization occurs if this option is selected the GPU will overestimate which pixels
+                // should be filled and which not. This only works with PolygonMode::Fill!
+                // Enabling this requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            // We are not using a depth/stencil buffer so we don't need this for now
+            depth_stencil: None,
+            // Multisampling/Anti-aliasing are very complex topics: https://www.youtube.com/watch?v=pFKalA-fd34
+            multisample: MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+
         Self {
             surface,
             device,
             queue,
             config,
             size,
+            render_pipeline,
         }
     }
 
@@ -122,7 +235,7 @@ impl State {
         // Now we need to actually clear the screen, to do that we will use a
         // "render pass" or in simple terms a single execution of the render
         // pipeline that we've built
-        let render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+        let mut render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
             label: Some("Render Pass"),
             color_attachments: &[Some(RenderPassColorAttachment {
                 view: &view,
@@ -139,6 +252,9 @@ impl State {
             })],
             depth_stencil_attachment: None,
         });
+
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.draw(0..3, 0..1);
         // The reason we drop the render_pass is because the command
         // encoder.begin_render_pass() borrows the encoder as &mut which
         // means that we can't call encoder.finish() until that borrow is given
